e95003924385db8d57dec6ab6c090097ab012d7f
Jason Shen <1275796924@qq.com>
2024-05-28 15:45:18 +0800

    feat(*):init project

---
+++ content/me.md
@@ -0,0 +1,5 @@
+# Jason Shen
+
+I'm Jason Shen 😪, a full-stack developer
+
+My blog is where I post my work-in-progress ideas, practice notes, and explorations in my free time
---
+++ site.config.ts
@@ -0,0 +1,79 @@
+export const siteConfig = {
+  author: "Jason Shen",
+  title: "Jason Shen - Blog",
+  description: "My blog site.",
+  lang: "zh-CN",
+};
+
+export const subNavLinks = [
+  {
+    title: "Blog",
+    path: "/blog",
+  },
+  {
+    title: "Life",
+    path: "/life",
+  },
+  {
+    title: "Record",
+    path: "/record",
+  },
+];
+
+export const navLinks = [
+  {
+    title: "Blog",
+    path: "/blog",
+    icon: "i-icon-park-outline-align-text-right-one",
+  },
+  {
+    title: "Projects",
+    path: "/projects",
+    icon: "i-icon-park-outline-blocks-and-arrows",
+  },
+  {
+    title: "Tags",
+    path: "/tags",
+    icon: "i-icon-park-outline-tag-one",
+  },
+  {
+    title: "Search",
+    path: "/search",
+    icon: "i-icon-park-outline-search",
+  },
+  {
+    title: "About",
+    path: "/",
+    icon: "i-icon-park-outline-grinning-face-with-open-mouth",
+  },
+];
+
+export const socialLinks = [
+  {
+    title: "Github",
+    path: "https://github.com/hayyot",
+    icon: "i-icon-park-outline-github",
+  },
+];
+
+export const projectList = [
+  {
+    name: "Projects",
+    content: [],
+  },
+];
+// {
+//   name: "Nuxt Blog",
+//   desc: "My blog site, base on Nuxt",
+//   path: "https://github.com/chansee97/nuxt-blog",
+// },
+// {
+//   name: "Nova Admin",
+//   desc: "a complete admin template",
+//   path: "https://github.com/chansee97/nova-admin",
+// },
+// {
+//   name: "Nova Admin Nest",
+//   desc: "The nest backend for nova admin",
+//   path: "https://github.com/chansee97/nove-admin-nest",
+// },
